91d4de531ba921acfca3b544222bf5d9bf4056be
Reshama Shaikh <reshama.stat@gmail.com>
2026-05-23 10:16:17 -0400

    Add Google Analytics  (#179)
    * adding GA code
    
    * tested GA pick-up in dev mode

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        provider: "ga",
+        id: "G-E5SMJ9E985",
+      },
+    }),
+  ],
   base: "/",
   test: {
     environment: "jsdom",
